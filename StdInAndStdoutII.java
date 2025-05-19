2a8d1a03c477f3ca5a55649d7fd9159556166e01
mepriyansh <priyanshb1234@gmail.com>
2025-05-19 15:01:51 +0530

    StdInAndStdout2

--- StdInAndStdoutII.java
+++ StdInAndStdoutII.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class StdInAndStdout {
+public class StdInAndStdoutII {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);

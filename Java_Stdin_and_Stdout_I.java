6f04cb84ce2101126c941b295c0de1f7836b596d
mepriyansh <priyanshb1234@gmail.com>
2025-05-19 14:51:37 +0530

    Welcome to Java and Stdin and Stdout 1

---
+++ Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Java_Stdin_and_Stdout_I  {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a  = scan.nextInt();
+        int b = scan.nextInt();
+        int c  = scan.nextInt();
+        // Complete this line
+        // Complete this line
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
